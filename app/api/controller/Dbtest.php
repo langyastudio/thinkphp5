d5aa4314fa8ef57a54bdd83f49cd3e84ef74d4fb
jiangjiaxiong <jiangjiaxiong@hacfin.com>
2017-01-25 12:54:58 +0800

    【tp - 数据库】测试：查询、添加、更新、删除、查询方法、查询语法。

--- app/api/controller/Dbtest.php
+++ app/api/controller/Dbtest.php
@@ -2,7 +2,7 @@
 namespace app\api\controller;
 use think\Controller;
 use think\Db;
-use think\Request;
+
 
 /**
  * Index
@@ -24,7 +24,8 @@ class Dbtest extends Controller
 
 	}
 
-	public function Index()
+	// 事务
+	public function Trans()
 	{
 		try
 		{
@@ -35,10 +36,157 @@ class Dbtest extends Controller
 			// 提交事务
 			Db::commit();
 			return $num;
-		} catch (\Exception $e)
+		}
+		catch (\Exception $e)
 		{
 			// 回滚事务
 			Db::rollback();
 		}
+	}
+
+	// 基本查询
+	public function QueryBase()
+	{
+		//1.0 基本查询
+		//查询数据集
+		dump(Db::name('user')
+				->where('id', 1)
+				->select());
+
+		//查询单条数据
+		dump( Db::name('user')
+		->where('id', 2)
+		->find());
+	}
+
+	// 查询列
+	public function QueryColumn()
+	{
+		// 某个字段的值
+		dump( Db::name('user')
+				->where('id', '<', 4)
+				->value('name'));
+
+		// 某个列的数组
+		dump( Db::name('user')
+		->where('id', '<', 4)
+		->column('name'));
+	}
+
+	// 查询列 - where方法
+	public function QueryWhere()
+	{
+		// AND
+		dump(Db::name('user')
+				->where('id', '>', 3)
+				->where('email', 'LIKE', '123%')
+				->select()
+		);
+	}
+
+	// 查询列 - where方法
+	public function QueryWhereOr()
+	{
+		// or
+		dump(Db::name('user')
+				->where('id', '>', 3)
+				->whereOr('email', 'LIKE', '123%')
+				->select()
+		);
+	}
+
+	// 查询列 - 混合查询
+	public function QueryMix()
+	{
+		dump(
+				Db::name('user')
+						->where(function ($query)
+						{
+							$query->where('id', 1)->whereOr('id', 2);
+						})->whereOr(function ($query)
+						{
+							$query->where('email', 'LIKE', '123%')->whereOr('name', 'LIKE', 'test%');
+						})->select()
+		);
+	}
+
+	// 添加单条数据
+	public function InsertOne()
+	{
+		$data = [
+				'name'  => 'jiangjiaxiong',
+				'pwd'   => '123456',
+				'email' => '12315@qq.com'
+		];
+
+		$rtn = Db::name('user')->insert($data);
+		return Db::name('user')->getLastInsID();
+	}
+
+	// 添加多条
+	public function InsertAll()
+	{
+		$data = [
+				[
+						'name'  => 'jiangjiaxiong',
+						'pwd'   => '123456',
+						'email' => '12315@qq.com'
+				],
+				[
+						'name'  => 'testuser',
+						'pwd'   => '123456',
+						'email' => 'test@qq.com'
+				]
+		];
+
+		$rtn = Db::name('user')->insertAll($data);
+
+		return json($rtn);
+	}
+
+	// 更新数据表中的数据
+	public function UpdateOne()
+	{
+		$rtn = Db::name('user')
+				->where('id', 1)
+				->update([
+						'name'        => 'nameupdate',
+						'create_time' => ['exp', 'now()']
+				]);
+
+		return json($rtn);
+	}
+
+	// 自增或自减一个字段的值
+	public function UpdateInc()
+	{
+		$rtn = Db::name('user')
+				->where('id', 1)
+				->setInc('sex'); //sex 本身必须是一个int值，不能是NULL （setDec）
+
+		return json($rtn);
+	}
+
+	public function Delete()
+	{
+		$rtn = Db::name('user')
+				->delete('10, 11, 12');
+
+		return json($rtn);
+	}
+
+	public function Aggregation()
+	{
+
+	}
+
+	public function ViewQuery()
+	{
+
+	}
+
+	public function SubQuery()
+	{
+
 	}
 }
