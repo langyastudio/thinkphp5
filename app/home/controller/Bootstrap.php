c165ea5253d28418c2d010c02cbc7dd2dd9f927b
jiangjiaxiong <jiangjiaxiong@hacfin.com>
2017-04-14 13:06:32 +0800

    新增 Bootstrap button Demo

---
+++ app/home/controller/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\home\controller;
+
+/**
+ * Bootstrap 相关应用
+ *
+ * @package app\home\controller
+ */
+class Bootstrap
+{
+    public function Button()
+    {
+		return view('bootstrap/Button');
+    }
+}
