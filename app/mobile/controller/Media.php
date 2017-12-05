54e70ccf58ab98dddfb76a6805905c79e5b915b3
jiangjiaxiong <1032030048@qq.com>
2017-12-05 16:06:01 +0800

    新增手持端html5-video，禁用下载按钮测试。（发现搜狗浏览器仍然未解决）

---
+++ app/mobile/controller/Media.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\mobile\controller;
+
+/**
+ * 音频和视频、幻灯片和轮播图、图片展示、图像、地图、滑块和旋转
+ */
+class Media
+{
+    public function Video()
+    {
+        return view('media/video');
+    }
+}
